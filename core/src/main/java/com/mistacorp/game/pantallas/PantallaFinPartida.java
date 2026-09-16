3b7a3e07e195397c90743691ac0a1e3f022e42c6
l-sudo-l <luis.manuel.rodriguez.0827@gmail.com>
2026-09-16 23:59:34 +0000

    feat(pantallas): banners e imagenes de boton en PantallaFinPartida
    - Reemplaza el texto 'GANADOR: X' por el banner fijo correspondiente
      (ganador_jugador1.png o ganador_jugador2.png segun quien gano).
    - Dibuja los botones volver_jugar.png / volver_menu.png (no son
      clickeables porque el juego es 100% teclado, pero se etiquetan con
      ENTER / ESC debajo de cada uno para que quede claro que tecla hace
      que accion).
    - ENTER arranca una partida nueva (PantallaJuego); ESC vuelve al menu
      (PantallaMenu). Antes solo existia la opcion de volver al menu.
    - Reproduce sonidoGanar una vez al entrar a la pantalla, usando
      ControlVolumen.obtenerVolumenEfectivo().

--- core/src/main/java/com/mistacorp/game/pantallas/PantallaFinPartida.java
+++ core/src/main/java/com/mistacorp/game/pantallas/PantallaFinPartida.java
@@ -5,16 +5,20 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mistacorp.game.ConfiguracionJuego;
+import com.mistacorp.game.ControlVolumen;
 import com.mistacorp.game.KatapumPrincipal;
+import com.mistacorp.game.RecursosGraficos;
 
 public class PantallaFinPartida extends ScreenAdapter {
 
     private final KatapumPrincipal juego;
     private final String nombreGanador;
+    private final Texture texturaBanner;
 
     private final OrthographicCamera camara = new OrthographicCamera();
     private final Viewport ventanaGrafica = new FitViewport(ConfiguracionJuego.ANCHO_MUNDO, ConfiguracionJuego.ALTO_MUNDO, camara);
@@ -22,14 +26,25 @@ public class PantallaFinPartida extends ScreenAdapter {
     public PantallaFinPartida(KatapumPrincipal juego, String nombreGanador) {
         this.juego = juego;
         this.nombreGanador = nombreGanador;
+
+        RecursosGraficos recursos = juego.obtenerRecursos();
+        this.texturaBanner = "Jugador 1".equals(nombreGanador)
+            ? recursos.obtenerTexturaGanadorJugador1()
+            : recursos.obtenerTexturaGanadorJugador2();
     }
 
     @Override
     public void show() {
+        juego.obtenerSonidos().obtenerSonidoGanar().play(ControlVolumen.obtenerVolumenEfectivo());
+
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean keyDown(int keycode) {
                 if (keycode == Input.Keys.ENTER) {
+                    juego.setScreen(new PantallaJuego(juego));
+                    return true;
+                }
+                if (keycode == Input.Keys.ESCAPE) {
                     juego.setScreen(new PantallaMenu(juego));
                     return true;
                 }
@@ -52,10 +67,25 @@ public class PantallaFinPartida extends ScreenAdapter {
         juego.obtenerLote().setProjectionMatrix(camara.combined);
         juego.obtenerLote().begin();
 
-        juego.obtenerFuente().draw(juego.obtenerLote(), "GANADOR: " + nombreGanador,
-            ConfiguracionJuego.ANCHO_MUNDO / 2f - 150, ConfiguracionJuego.ALTO_MUNDO / 2f + 20);
-        juego.obtenerFuente().draw(juego.obtenerLote(), "Presiona ENTER para volver al menu",
-            ConfiguracionJuego.ANCHO_MUNDO / 2f - 190, ConfiguracionJuego.ALTO_MUNDO / 2f - 20);
+        float anchoBanner = 420f;
+        float altoBanner = 110f;
+        float xBanner = ConfiguracionJuego.ANCHO_MUNDO / 2f - anchoBanner / 2f;
+        float yBanner = 300f;
+        juego.obtenerLote().draw(texturaBanner, xBanner, yBanner, anchoBanner, altoBanner);
+
+        RecursosGraficos recursos = juego.obtenerRecursos();
+        float anchoBoton = 220f;
+        float altoBoton = 60f;
+        float separacion = 40f;
+        float xBotones = ConfiguracionJuego.ANCHO_MUNDO / 2f - (anchoBoton * 2 + separacion) / 2f;
+        float yBotones = 180f;
+
+        juego.obtenerLote().draw(recursos.obtenerTexturaBotonVolverJugar(), xBotones, yBotones, anchoBoton, altoBoton);
+        juego.obtenerLote().draw(recursos.obtenerTexturaBotonVolverMenu(),
+            xBotones + anchoBoton + separacion, yBotones, anchoBoton, altoBoton);
+
+        juego.obtenerFuente().draw(juego.obtenerLote(), "ENTER", xBotones + 80, yBotones - 12);
+        juego.obtenerFuente().draw(juego.obtenerLote(), "ESC", xBotones + anchoBoton + separacion + 90, yBotones - 12);
 
         juego.obtenerLote().end();
     }
